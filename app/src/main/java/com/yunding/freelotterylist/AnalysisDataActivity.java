34b19633092afcb2551b70aa81d293b9989d36b1
arctan <769242784@qq.com>
2018-03-23 18:59:15 +0800

    sadasdasasd

---
+++ app/src/main/java/com/yunding/freelotterylist/AnalysisDataActivity.java
@@ -0,0 +1,13 @@
+package com.yunding.freelotterylist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnalysisDataActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis_data);
+    }
+}
